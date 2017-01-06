d1a7b36669bb8d8c3e44505e28bed3f06e5fb61a
Lijun <lijunboy00@163.com>
2017-01-06 14:13:36 +0800

    测试

--- index.js
+++ index.js
@@ -6,10 +6,10 @@
 const fs = require("fs");
 const path = require("path");
 module.exports = function (dirPath, app) {
-    let files = fs.readdirSync(dirPath);
+    let files = fs.readdirSync(dirPath)
     files.forEach(function (item, i) {
         let rootPath = "/" + item.split(".")[0]
-        app.use(rootPath, require(dirPath + path.sep + item));
+        app.use(rootPath, require(dirPath + path.sep + item))
     })
 }
 
